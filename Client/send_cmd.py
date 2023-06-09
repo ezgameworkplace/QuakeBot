0b5f1f07232db3413d998b1d07a5d9045305de73
ezgamer <alexji888@outlook.com>
2023-06-09 17:27:30 +0800

    add client

---
+++ Client/send_cmd.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+File:send_cmd.py
+Author:ezgameworkplace
+Date:2023/6/9
+"""
+import json
+import socket
+import time
+from typing import Dict
+
+
+def send_command_to_unity(command:Dict):
+    # 创建一个socket对象
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # 连接到你的Unity服务器，这里假设服务器运行在本地机器上的9999端口
+    s.connect(('localhost', 9999))
+
+    # 发送你的命令, 将字典转换为JSON字符串
+    json_str = json.dumps(command)
+    s.sendall(json_str.encode('utf-8'))
+
+    # 接收服务器的响应
+    data = s.recv(1024)
+    print("Received: " + data.decode('utf-8'))
+    # 关闭连接
+    s.close()
+
+
+if __name__ == '__main__':
+
+    # 用你的命令替换 "your_command_here"
+    dict_cmd = {
+        "command": "ClickButton",
+        "parameters": "Attack"
+    }
+    while True:
+        time.sleep(0.5)
+        send_command_to_unity(dict_cmd)
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# QuakeBot
-A tester for game
+# QuakeBot 
+Let's test game
+## Purpose
+To create a bot that can test game by itself in Unity  
+
+
+## Features
+|     | Task                                                                         | Status |
+|-----|------------------------------------------------------------------------------|--------|
+| 1.  | Connect Game and Bot - using a socket to control game play                   | <span style="color:lightgreen;">Done</span>   |
+| 2.  | Add Commands Nessesary for Test                                              |    |
+| 3.  | Let Bot Write Code For Test - scan all gameObjects and parse their functions |    |
+
+
+
+
+---------------- 
+
+
+
+## Installation
+1. Python 3.10
+2. Unity 2021.3.25f1
+
+## How to use
+TODO
